27bc258bb13fa2edfd5380db5e46e2c3ba7cbdf5
Marcus Paulo <mpaulobr@gmail.com>
2015-01-21 23:09:26 -0200

    Ajuste no Layout e adição da Funcionalidade de Compartilhamento no Twitter e Facebook

--- PalestrasColetivas/Controlador/DetalheEventoViewController.swift
+++ PalestrasColetivas/Controlador/DetalheEventoViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Social
 
 class DetalheEventoViewController: UIViewController {
     
@@ -24,10 +25,25 @@ class DetalheEventoViewController: UIViewController {
     
     @IBOutlet weak var tipoMapaSegmentedControl: UISegmentedControl!
     
+    @IBOutlet var toolbar: UIToolbar!
+    
+    @IBOutlet var textView: UITextView!
+    
+//    lazy var activityPopover:UIPopoverController = {
+//        return UIPopoverController(contentViewController: self.activityViewController)
+//        }()
+    
+    lazy var activityViewController:UIActivityViewController = {
+        return self.createActivityController()
+        }()
+
+    
     override func viewDidLoad() {
-        println(evento.name);
         
-        tituloDetalheEventoLabel.title = evento.name
+        descricaoTextView.layer.cornerRadius = 5
+        
+        
+//        tituloDetalheEventoLabel.title = evento.name
         descricaoTextView.text = evento.description
         dataInicioLabel.text = evento.fullDate
         enderecoLabel.text = evento.street
@@ -43,6 +59,114 @@ class DetalheEventoViewController: UIViewController {
             
         default: mapa.mapType = MKMapType.Standard;
             
+        }
+    }
+    
+    @IBAction func shareAction(sender: AnyObject) {
+        share(sender)
+    }
+    
+    func share(sender: AnyObject) {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            self.navigationController?.presentViewController(activityViewController, animated: true, completion: nil)
+        }
+//        else if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+//            // actually, you don't have to do this. But if you do want a popover, this is how to do it.
+//            iPad(sender)
+//        }
+    }
+    
+//    func iPad(sender: AnyObject) {
+//        if !self.activityPopover.popoverVisible {
+//            if sender is UIBarButtonItem {
+//                self.activityPopover.presentPopoverFromBarButtonItem(sender as UIBarButtonItem,
+//                    permittedArrowDirections:.Any,
+//                    animated:true)
+//            } else {
+//                var b = sender as UIButton
+//                self.activityPopover.presentPopoverFromRect(b.frame,
+//                    inView: self.view,
+//                    permittedArrowDirections:.Any,
+//                    animated:true)
+//            }
+//        } else {
+//            self.activityPopover.dismissPopoverAnimated(true)
+//        }
+//    }
+    
+    func createActivityController() -> UIActivityViewController {
+        
+        
+        let url:NSURL = NSURL(string:"http://palestrascoletivas.com")!
+        
+        
+        // let's add a String and an NSURL
+        var activityViewController = UIActivityViewController(
+            activityItems: [evento.name + " #PalestrasColetivas", url],
+            applicationActivities: nil)
+        
+        activityViewController.completionHandler = {(activityType, completed:Bool) in
+            if !completed {
+                println("cancelled")
+                return
+            }
+            
+            if activityType == UIActivityTypePostToTwitter {
+                println("twitter")
+            }
+            
+            if activityType == UIActivityTypePostToFacebook {
+                println("Facebook")
+            }
+            
+//            if activityType == UIActivityTypeMail {
+//                println("mail")
+//            }
+        }
+        
+        // you can specify these if you'd like.
+        activityViewController.excludedActivityTypes =  [
+//                    UIActivityTypePostToTwitter,
+//                    UIActivityTypePostToFacebook,
+                    UIActivityTypePostToWeibo,
+                    UIActivityTypeMessage,
+                    UIActivityTypeMail,
+                    UIActivityTypePrint,
+                    UIActivityTypeCopyToPasteboard,
+                    UIActivityTypeAssignToContact,
+                    UIActivityTypeSaveToCameraRoll,
+                    UIActivityTypeAddToReadingList,
+                    UIActivityTypePostToFlickr,
+                    UIActivityTypePostToVimeo,
+                    UIActivityTypePostToTencentWeibo
+        ]
+        
+        return activityViewController
+    }
+
+
+    
+    @IBAction func twitterButtonPushed(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter){
+            var twitterSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            twitterSheet.setInitialText(evento.name + " #PalestrasColetivas")
+            self.presentViewController(twitterSheet, animated: true, completion: nil)
+        } else {
+            var alert = UIAlertController(title: "Atenção", message: "Para compartilhar, é necessário logar no Twitter", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func facebookButtonPushed(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook){
+            var facebookSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            facebookSheet.setInitialText(evento.name + " #PalestrasColetivas")
+            self.presentViewController(facebookSheet, animated: true, completion: nil)
+        } else {
+            var alert = UIAlertController(title: "Atenção", message: "Para compartilhar, é necessário logar no Facebook", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
         }
     }
 }
